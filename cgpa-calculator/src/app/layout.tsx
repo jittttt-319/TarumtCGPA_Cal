21c0fcca57bf227b412dba2064a79ddd54e52d92
hehe <jitxuan2021@gmail.com>
2025-05-04 09:40:12 +0800

    ..

--- cgpa-calculator/src/app/layout.tsx
+++ cgpa-calculator/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] });
 
